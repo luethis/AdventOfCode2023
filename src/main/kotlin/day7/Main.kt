bb1c4488afbaef0620595cbe23f26287567c3020
Serafin Lüthi <serafin.luethi@outlook.com>
2023-12-08 22:32:57 +0100

    finish part 2 day 7

--- src/main/kotlin/day7/Main.kt
+++ src/main/kotlin/day7/Main.kt
@@ -4,7 +4,9 @@ import java.io.File
 
 val input = File("src/main/kotlin/day7/input.txt")
 
-val handRanking = mapOf(
+val isPart2 = true
+
+private val handRanking = mapOf(
     7 to listOf(5), // five of a kind
     6 to listOf(4, 1), // four of a kind
     5 to listOf(3, 2), // full house
@@ -14,8 +16,24 @@ val handRanking = mapOf(
     1 to listOf(1, 1, 1, 1, 1) // high card
 )
 
-val charMapping = mapOf(
-    "1" to "1",
+private fun calculateJokerRank(rank: Int, jokerCount: Int) =
+    when (rank to jokerCount) {
+        1 to 1 -> 2
+        1 to 2 -> 4
+        1 to 3 -> 6
+        1 to 4 -> 7
+        1 to 5 -> 7
+        2 to 1 -> 4
+        2 to 2 -> 6
+        2 to 3 -> 7
+        3 to 1 -> 5
+        4 to 1 -> 6
+        4 to 2 -> 7
+        6 to 1 -> 7
+        else -> rank
+    }
+
+private val charMapping = mapOf(
     "2" to "2",
     "3" to "3",
     "4" to "4",
@@ -25,16 +43,18 @@ val charMapping = mapOf(
     "8" to "8",
     "9" to "9",
     "T" to "A",
-    "J" to "B",
+    "J" to if (isPart2) "1" else "B",
     "Q" to "C",
     "K" to "D",
     "A" to "E"
 )
 
+
 fun main() {
     val handComparator = Comparator { hand1: Hand, hand2: Hand ->
         when {
-            hand1.rank != hand2.rank -> hand1.rank.compareTo(hand2.rank)
+            isPart2 && hand1.jokerRank != hand2.jokerRank -> hand1.jokerRank.compareTo(hand2.jokerRank)
+            !isPart2 && hand1.rank != hand2.rank -> hand1.rank.compareTo(hand2.rank)
             else -> hand1.mappedCards.compareTo(hand2.mappedCards)
         }
     }
@@ -43,7 +63,8 @@ fun main() {
     input.forEachLine {
         val (cards, score) = it.split(" ")
         val rank = calculateRank(cards)
-        val hand = Hand(cards, score.toInt(), rank)
+        val jokerRank = calculateJokerRank(cards)
+        val hand = Hand(cards, score.toInt(), rank, jokerRank)
         hands.add(hand)
     }
 
@@ -62,6 +83,18 @@ fun calculateRank(cards: String): Int {
     return handRanking.entries.single { possibility -> possibility.value == comb }.key
 }
 
-data class Hand(val cards: String, val bid: Int, val rank: Int) {
+fun calculateJokerRank(cards: String): Int {
+    val jokerCount = cards.count { it == 'J' }
+    val normalizedCards = cards.replace("J", "")
+
+    val comb = normalizedCards.groupBy { it }.map { it.value.size }.sortedDescending().toMutableList()
+    while (comb.sumOf { it } != 5) {
+        comb.add(1)
+    }
+    val rank = handRanking.entries.single { possibility -> possibility.value == comb }.key
+    return calculateJokerRank(rank, jokerCount)
+}
+
+data class Hand(val cards: String, val bid: Int, val rank: Int, val jokerRank: Int) {
     val mappedCards = cards.mapNotNull { charMapping[it.toString()] }.joinToString(separator = "")
 }
