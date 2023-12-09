6d436ab82f46db3c1047d8fb1f22ffd6df9f0baa
Serafin Lüthi <serafin.luethi@outlook.com>
2023-12-09 14:59:45 +0100

    day 9 part 1

---
+++ src/main/kotlin/day9/Main.kt
@@ -0,0 +1,28 @@
+package day9
+
+import java.io.File
+
+val input = File("src/main/kotlin/day9/input.txt")
+
+
+// to low: 1938800260
+// too h : 1938800269
+
+fun main() {
+    val numbers = input.readLines().map { it.split(" ").map { it.toInt() } }
+
+    val result = numbers.sumOf { it ->
+        val seq = mutableListOf(it)
+        while (!seq.last().all { it == 0 }) {
+            val latestDiffs = seq.last().mapIndexedNotNull { index, value ->
+                seq.last().getOrNull(index + 1)?.let {
+                    it - value
+                }
+            }
+            seq.add(latestDiffs)
+        }
+        seq.sumOf { it.last() }
+    }
+
+    println(result)
+}
